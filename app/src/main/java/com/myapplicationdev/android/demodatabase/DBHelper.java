1bef18ff6960c0e9d6a8f40f3aa03107e7b00018
Wen-New <16022742@myrp.edu.sg>
2018-05-08 12:49:59 +0800

    get Task method

--- app/src/main/java/com/myapplicationdev/android/demodatabase/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/demodatabase/DBHelper.java
@@ -2,9 +2,12 @@ package com.myapplicationdev.android.demodatabase;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -58,5 +61,39 @@ public class DBHelper extends SQLiteOpenHelper {
         // Close the database connection
         db.close();
     }
+
+    public ArrayList<String> getTaskContent() {
+        // Create an ArrayList that holds String objects
+        ArrayList<String> tasks = new ArrayList<String>();
+        // Select all the tasks' description
+        String selectQuery = "SELECT " + COLUMN_DESCRIPTION
+                + " FROM " + TABLE_TASK;
+
+        // Get the instance of database to read
+        SQLiteDatabase db = this.getReadableDatabase();
+        // Run the SQL query and get back the Cursor object
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // moveToFirst() moves to first row
+        if (cursor.moveToFirst()) {
+            // Loop while moveToNext() points to next row
+            //  and returns true; moveToNext() returns false
+            //  when no more next row to move to
+            do {
+                // Add the task content to the ArrayList object
+                //  0 in getString(0) return the data in the first
+                //  column in the Cursor object. getString(1)
+                //  return second column data and so on.
+                //  Use getInt(0) if data is an int
+                tasks.add(cursor.getString(0));
+            } while (cursor.moveToNext());
+        }
+        // Close connection
+        cursor.close();
+        db.close();
+
+        return tasks;
+    }
+
 
 }
--- app/src/main/java/com/myapplicationdev/android/demodatabase/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demodatabase/MainActivity.java
@@ -2,12 +2,17 @@ package com.myapplicationdev.android.demodatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnInsert;
+    Button btnInsert, btnGetTasks;
+    TextView tvResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnInsert = (Button) findViewById(R.id.buttonInsert);
+        btnGetTasks = (Button) findViewById(R.id.buttonGetTasks);
+        tvResults = (TextView) findViewById(R.id.textViewResults);
+
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +32,26 @@ public class MainActivity extends AppCompatActivity {
                 db.close();
             }
         });
+
+        btnGetTasks.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                ArrayList<String> data = db.getTaskContent();
+                db.close();
+
+                String txt = "";
+                for (int i = 0; i < data.size(); i++) {
+                    Log.d("Database Content", i +". "+data.get(i));
+                    txt += i + ". " + data.get(i) + "\n";
+                }
+                tvResults.setText(txt);
+            }
+        });
+
     }
 }
